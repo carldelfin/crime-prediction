020a0a90b55458abbc53397bd7c721b1043540f7
Carl Delfin <22149864+carldelfin@users.noreply.github.com>
2022-03-15 14:27:42 +0100

    Update install_r.sh

--- install_r.sh
+++ install_r.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 # import proper key 
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 'E298A3A825C0D65DFD57CBB651716619E084DAB9'
 
 # add repository
-#echo "deb http://cloud.r-project.org/bin/linux/debian bullseye-cran40/" | sudo tee -a /etc/apt/sources.list
- 
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+
 # install r and some dependencies
 sudo apt update && sudo apt install -y r-base libnode-dev libxml2-dev libcurl4-openssl-dev libopenblas-dev
+
+# install r packages
+R --slave -e 'install.packages(c("tidyverse", "doParallel", "nnet", "kernlab", "ranger", "xgboost", "dbarts", "devtools"))'
+
+# make sure we get latest versions of these
+R --slave -e 'devtools::install_github("tidymodels/tidymodels")'
+R --slave -e 'devtools::install_github("tidymodels/tune")'
+R --slave -e 'devtools::install_github("tidymodels/finetune")'
+R --slave -e 'devtools::install_github("tidymodels/dials")'
