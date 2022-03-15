e57f1ebf1fbec47a3164ce047277054cf796b410
Carl Delfin <22149864+carldelfin@users.noreply.github.com>
2022-03-15 14:30:19 +0100

    Update install_r.sh

--- install_r.sh
+++ install_r.sh
@@ -3,7 +3,7 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 'E298A3A825C0D65DFD57CBB651716619E084DAB9'
 
 # add repository
-add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
 # install r and some dependencies
 sudo apt update && sudo apt install -y r-base libnode-dev libxml2-dev libcurl4-openssl-dev libopenblas-dev
