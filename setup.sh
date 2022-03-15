14dbfc42422f3df8dba7b0e9b94532b0d1e2412c
Carl Delfin <22149864+carldelfin@users.noreply.github.com>
2022-03-15 15:07:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,19 +8,19 @@ echo "deb http://cloud.r-project.org/bin/linux/debian bullseye-cran40/" | tee -a
 sudo apt update && sudo apt install -y r-base libnode-dev libxml2-dev libcurl4-openssl-dev libopenblas-dev
 
 # install required R packages
-R --slave -e 'dir.create(path = Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)'
-R --slave -e 'install.packages(c("tidyverse", "doParallel", "nnet", "kernlab", "ranger", "xgboost", "dbarts", "devtools"))'
-R --slave -e 'devtools::install_github("tidymodels/tidymodels")'
-R --slave -e 'devtools::install_github("tidymodels/tune")'
-R --slave -e 'devtools::install_github("tidymodels/finetune")'
-R --slave -e 'devtools::install_github("tidymodels/dials")'
+#R --slave -e 'dir.create(path = Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)'
+#R --slave -e 'install.packages(c("tidyverse", "doParallel", "nnet", "kernlab", "ranger", "xgboost", "dbarts", "devtools"))'
+#R --slave -e 'devtools::install_github("tidymodels/tidymodels")'
+#R --slave -e 'devtools::install_github("tidymodels/tune")'
+#R --slave -e 'devtools::install_github("tidymodels/finetune")'
+#R --slave -e 'devtools::install_github("tidymodels/dials")'
 
 # install TensorFlow and Keras
 sudo apt install -y python3-dev python3-pip python3-venv
-pip3 install tensorflow
-R --slave -e 'install.packages(c("tensorflow", "keras"))'
+#pip3 install tensorflow
+#R --slave -e 'install.packages(c("tensorflow", "keras"))'
 
 # install torch and brulee
-R --slave -e 'devtools::install_github("mlverse/torch")'
-R --slave -e 'torch::install_torch()'
-R --slave -e 'devtools::install_github("tidymodels/brulee")'
+#R --slave -e 'devtools::install_github("mlverse/torch")'
+#R --slave -e 'torch::install_torch()'
+#R --slave -e 'devtools::install_github("tidymodels/brulee")'
